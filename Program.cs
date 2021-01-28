28f8a0f182503ae207cfd9a2efe9f877f46a828e
BuildTools <unconfigured@null.spigotmc.org>
2021-01-28 15:09:20 -0500

    A Console Program
    Written in C# By MrPotatoMan56742#0001 and Non-Creative-Nickname#9574

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Retonex
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Change the Apperence
+            Console.Title = "Retonex";
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Welcome to Retonex!\nEnter a username bellow");
+
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Your username is now " + userName + "!");
+
+            Console.WriteLine(userName + " Type Launch when your ready to start the Launcher");
+
+            Console.ReadLine();
+
+            Console.Clear();
+
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("Retonex v 1.0.0");
+            Console.WriteLine("Loading...");
+
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Press any key to close the window...");
+
+            Console.ReadKey();
+        }
+    }
+}
